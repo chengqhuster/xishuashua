d9f7e7f71dc20648491cac40fae62251dbfc0f9c
chengqhuster <chengqhuster@gmail.com>
2018-08-29 23:52:21 +0800

    Add LeetCode FrogJump Java

--- src/main/LeetCode/java/FrogJump.java
+++ src/main/LeetCode/java/FrogJump.java
@@ -1,4 +1,62 @@
 package LeetCode.java;
 
+/*
+ * 题目描述：https://leetcode.com/problems/frog-jump/description/
+ *
+ * 思路简述：动态规划 每个节点（步骤）需要知道到达自身时青蛙上一次跳的步数[集合]（状态）
+ *          从第一步开始跳跃 1 步（起始条件），后续每次能跳 k-1, k, k+1 步（状态转移）
+ *          每次跳跃时将往可能跳至的目标点集合内加入此次跳跃距离，跳至终点结束
+ */
+
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FrogJump {
+    public boolean canCross(int[] stones) {
+        if(stones == null || stones.length == 0) {
+            return false;
+        }
+        LinkedHashMap<Integer, HashSet<Integer>> map = new LinkedHashMap<>();
+        for(int i=0; i<stones.length; i++) {
+            map.put(stones[i], new HashSet<>());
+        }
+        int target = stones[stones.length - 1];
+        for(Map.Entry<Integer, HashSet<Integer>> entry  : map.entrySet()) {
+            int startPoint = entry.getKey();
+
+            // 第一步只能跳 1 步数
+            if(entry.getKey() == stones[0]) {
+                if(jumpTry(startPoint, 1, map) == target) {
+                    return true;
+                }
+                continue;
+            }
+
+            // 其他情况跳 k-1, k , k+1 步数
+            for(int step : entry.getValue()) {
+                int jumpA = step - 1;
+                if(jumpA > 0 && jumpTry(startPoint, jumpA, map) == target) {
+                    return true;
+                }
+                if(jumpTry(startPoint, step, map) == target) {
+                    return true;
+                }
+                if(jumpTry(startPoint, step + 1, map) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // 跳跃成功时，返回落点
+    private int jumpTry(int startPoint, int step, LinkedHashMap<Integer, HashSet<Integer>> map) {
+        int endPoint = startPoint + step;
+        if(map.containsKey(endPoint)) {
+            map.get(endPoint).add(step);
+            return endPoint;
+        }
+        return 0;
+    }
 }
--- src/test/LeetCode/java/FrogJumpTest.java
+++ src/test/LeetCode/java/FrogJumpTest.java
@@ -1,4 +1,41 @@
 package LeetCode.java;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class FrogJumpTest {
+
+    private int[] input;
+
+    private FrogJump frogJump;
+
+    @Parameters
+    public static List getParams() {
+        int[] caseA = {0,1,3,5,6,8,12,17};
+        int[] caseB = {0,1,2,3,4,8,9,11};
+        int[] caseC = {0,2};
+        return Arrays.asList(caseA , caseB, caseC);
+    }
+
+    public FrogJumpTest(int[] input) {
+        this.input = input;
+    }
+
+    @Before
+    public void setUp() {
+        frogJump = new FrogJump();
+    }
+
+    @Test
+    public void SolutionTest() {
+        System.out.println("input:  " + Arrays.toString(input));
+        System.out.println("result: " + frogJump.canCross(input));
+    }
 }
