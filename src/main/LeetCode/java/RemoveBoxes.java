af2002df1a2b47a2a4ce49b5d742fd836a94f52b
chengqhuster <chengqhuster@gmail.com>
2019-05-21 15:52:11 +0800

    remove boxes

---
+++ src/main/LeetCode/java/RemoveBoxes.java
@@ -0,0 +1,14 @@
+package LeetCode.java;
+
+/*
+ * 题目描述：https://leetcode.com/problems/remove-boxes/
+ *
+ * 思路简述：参考 BurstBalloons，仔细思考 dp 怎么构造
+ *
+ */
+
+public class RemoveBoxes {
+    public int removeBoxes(int[] boxes) {
+        return 0;
+    }
+}
