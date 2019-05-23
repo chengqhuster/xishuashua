5034ded1d60b805d1af3496e555820aee525711c
chengqhuster <chengqhuster@gmail.com>
2019-05-23 18:22:44 +0800

    find eventual safe status

--- src/main/LeetCode/java/FindEventualSafeStatus.java
+++ src/main/LeetCode/java/FindEventualSafeStatus.java
@@ -5,6 +5,7 @@ package LeetCode.java;
  *
  * 思路简述：1. 判断每个节点，从该节点出发无环，则为所求节点，复杂度高
  *          2. 从已有结果集(出度为 0 的点)出发，往结果集节点的父节点遍历计算
+ *          3. 继承1的思路，但是在dfs时需要给路过的节点加上状态
  *
  */
 
@@ -13,7 +14,43 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-public class FindEventualSafeStatus {
+public class FindEventualSafeStatus{
+    final static int COLOR_BLANK = 0;
+    final static int COLOE_RED = 1;
+    final static int COLOR_BLUE = 2;
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> res = new ArrayList<>();
+        if (graph == null || graph.length == 0) {
+            return res;
+        }
+        int[] color = new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == COLOR_BLANK) {
+                dfs(graph, color, res, i);
+            }
+        }
+        Collections.sort(res);
+        return res;
+    }
+
+    private boolean dfs(int[][] graph, int[] color, List<Integer> res, int pos) {
+        if (color[pos] != COLOR_BLANK) {
+            return color[pos] == COLOR_BLUE;
+        }
+        color[pos] = COLOE_RED;
+        for (int next : graph[pos]) {
+            if (!dfs(graph, color, res, next)) {
+                return false;
+            }
+        }
+        color[pos] = COLOR_BLUE;
+        res.add(pos);
+        return true;
+    }
+}
+
+class FindEventualSafeStatusTLE {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> res = new ArrayList<>();
         HashMap<Integer, GraphNode> map = new HashMap<>();
