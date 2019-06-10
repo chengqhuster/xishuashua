c2db9b48fd3181078a7573fb97f460ab00918d4f
chengqhuster <chengqhuster@gmail.com>
2019-06-10 20:00:27 +0800

    most stones removed with same row or column [union find]

--- src/main/LeetCode/java/MostStonesRemovedWithSameRowOrColumn.java
+++ src/main/LeetCode/java/MostStonesRemovedWithSameRowOrColumn.java
@@ -6,6 +6,9 @@ package LeetCode.java;
  * 思路简述： 参考 https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/discuss/197668/
  *              https://www.jianshu.com/p/30d2058db7f7
  *
+ *          1.dfs 的思想找出所有横纵坐标相同的节点（组成 island）, 行列可以统一到一个维度（比如 column 的范围是 N ~ 2N-1）
+ *          2.横纵坐标统一后，可以使用并查集的思想（纵坐标直接映射到负数空间）
+ *
  */
 
 import java.util.HashMap;
@@ -60,6 +63,37 @@ public class MostStonesRemovedWithSameRowOrColumn {
       if (!rDfs.contains(i)) {
         rowDfs(i);
       }
+    }
+  }
+
+  HashMap<Integer, Integer> map = new HashMap<>();
+  int islands = 0;
+
+  public int removeStonesUF(int[][] stones) {
+    for (int[] pos : stones) {
+      union(pos[0], ~pos[1]);
+    }
+    return stones.length - islands;
+  }
+
+  public int find(int x) {
+    // putIfAbsent return old value
+    if (map.putIfAbsent(x, x) == null) {
+      islands++;
+    }
+    if (x != map.get(x)) {
+      //reduce depth
+      map.put(x, find(map.get(x)));
+    }
+    return map.get(x);
+  }
+
+  public void union(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if (x != y) {
+      map.put(x, y);
+      islands--;
     }
   }
 }
