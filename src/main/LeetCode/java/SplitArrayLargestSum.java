b7c37a99091ca0dfad1220ef9c3313c4421df116
chengqhuster <chengqhuster@gmail.com>
2019-07-05 22:03:26 +0800

    split array largest sum

--- src/main/LeetCode/java/SplitArrayLargestSum.java
+++ src/main/LeetCode/java/SplitArrayLargestSum.java
@@ -3,7 +3,8 @@ package LeetCode.java;
 /*
  * 题目描述：https://leetcode.com/problems/split-array-largest-sum/
  *
- * 思路简述：参考 Largest Sum of average
+ * 思路简述：a.参考 Largest Sum of average
+ *           b.二分法
  *
  */
 
@@ -40,5 +41,53 @@ public class SplitArrayLargestSum {
         }
         dp[start][m] = min;
         return dp[start][m];
+    }
+
+    // 二分法的上下界 l r 分别代表结果可能的最小值与最大值“减一”，可能有个疑问是每次取值都取 mid + 1 或者 mid - 1 作为上下界，
+    // 如果结果刚好取 mid 怎么办，这种情况最终 l = r = 'mid' 再经过一次判断 l = 'mid' + 1 便取回了刚才的 mid 值
+    // 或者判断条件改为
+
+//    while (l < r) {
+//        long mid = (l + r) / 2;
+//        if (isValid(nums, mid, m)) {
+//            r = mid;
+//        } else {
+//            l = mid + 1;
+//        }
+//    }
+
+    public int splitArrayII(int[] nums, int m) {
+        if (nums == null || nums.length == 0 || nums.length < m) {
+            return 0;
+        }
+        long l = 0, r = 0;
+        for (int num : nums) {
+            l = Math.max(l, num);
+            r += num;
+        }
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            if (isValid(nums, mid, m)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int)l;
+    }
+
+    private boolean isValid(int[] nums, long group, int m) {
+        int count = 0, total = 0;
+        for (int num : nums) {
+            total += num;
+            if (total > group) {
+                count++;
+                total = num;
+                if (count >= m) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
