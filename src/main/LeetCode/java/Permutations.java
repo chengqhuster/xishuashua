cda3d2921f2234a12855526c8c3b8b88211ebbc2
chengqhuster <chengqhuster@gmail.com>
2021-10-12 12:27:13 +0800

    permutations [update]

--- src/main/LeetCode/java/Permutations.java
+++ src/main/LeetCode/java/Permutations.java
@@ -3,11 +3,14 @@ package LeetCode.java;
 /*
  * 题目描述：https://leetcode.com/problems/permutations/
  *
- * 思路简述：采用回溯的方式遍历
+ * 思路简述：1. 采用回溯的方式遍历
+ *         2. 采用交换的方式，递归实现
+ *         3. 采用交换的方式，非递归实现（模拟栈上的元素）
  *
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Permutations {
@@ -33,5 +36,71 @@ public class Permutations {
                 }
             }
         }
+    }
+
+    public List<List<Integer>> permuteRecursion(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 2) {
+            return res;
+        }
+        permuteRecursionCore(nums, res, 0);
+        return res;
+    }
+
+    private void permuteRecursionCore(int[] nums, List<List<Integer>> res, int pos) {
+        if (pos == nums.length) {
+            res.add(toList(nums));
+            return;
+        }
+        for (int i = pos; i < nums.length; i++) {
+            swap(nums, pos, i);
+            permuteRecursionCore(nums, res, pos + 1);
+            swap(nums, pos, i);
+        }
+    }
+
+    public List<List<Integer>> permuteNoRecursion(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 2) {
+            return res;
+        }
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(0);
+        while (!stack.isEmpty()) {
+            if (stack.size() == nums.length) {
+                res.add(toList(nums));
+                while (!stack.isEmpty() && stack.peek() == nums.length - 1) {
+                    stack.pop();
+                    swap(nums, stack.size(), nums.length - 1);
+                }
+                if (!stack.isEmpty()) {
+                    int a = stack.pop();
+                    swap(nums, a, stack.size());
+                    stack.push(a + 1);
+                    swap(nums, a + 1, stack.size() - 1);
+                }
+            } else {
+                stack.push(stack.size());
+            }
+        }
+        return res;
+    }
+
+    private List<Integer> toList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        return list;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        if (a == b) {
+            return;
+        }
+        // 位运算实现交换
+        nums[a] = nums[a] ^ nums[b];
+        nums[b] = nums[a] ^ nums[b];
+        nums[a] = nums[a] ^ nums[b];
     }
 }
